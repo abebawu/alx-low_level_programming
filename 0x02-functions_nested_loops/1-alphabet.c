3fb20110dad3aba4caf1b9beb67e19f7ad77616b
abebawu <78111430+abebawu@users.noreply.github.com>
2022-06-07 08:10:26 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  *
@@ -8,6 +7,7 @@
 void print_alphabet(void)
 {
 	int i;
+	
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
