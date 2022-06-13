3fcc35d8bf4f9c09d1fd58370d27ee3e3b13ea7a
abebawu <kuratghnawu@gmail.com>
2022-06-13 15:10:38 -0700

    five answer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
    * rev_string - prints reversed string, followed by a new line
-    * @s: pointer to the string to print
+    * @s: pointeir to the string to print
      * Return: void
  */
 void rev_string(char *s)
 {
 int len, i, half;
 char temp;
-for (len = 0; s[len] != '\0'; len++);
+for (len = 0; s[len] != '\0'; len++)
+	;
 i = 0;
 half = len / 2;
 while (half--)
