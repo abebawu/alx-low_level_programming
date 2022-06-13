1323b34658bb1a91506bfa1e378ead9d44238627
abebawu <kuratghnawu@gmail.com>
2022-06-13 14:44:15 -0700

    lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * int-strlen - string length
+ * @a: first char
+ * Description: find the lenth of string
+ * Return: alwys (0)
+ */
+int _strlen(char *s)
+{
+strlen(*s);
+}
