83411c26af5aa4cd464a9d19650d897580007b6b
abebawu <kuratghnawu@gmail.com>
2022-06-13 14:37:51 -0700

    first

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,8 +6,8 @@
  *Description: swap the value of two integeres
  *Return: always (0)
  */
-void swap_int(int *a, int *b) {
-
+void swap_int(int *a, int *b)
+{
 *a = *a + *b;
 *b = *a - *b;
 *a = *a - *b;
