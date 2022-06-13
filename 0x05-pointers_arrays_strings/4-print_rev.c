f1a298bd57edd4832973793da8a8e8e5e7600e7a
abebawu <kuratghnawu@gmail.com>
2022-06-13 15:00:58 -0700

    fourth question'

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+   * print_rev - print reverse
+    * @s: input string
+    * Description:  prints a string, in revers
+      * Return: Always (0)
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
