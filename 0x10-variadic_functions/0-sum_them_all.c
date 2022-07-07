6e215955dedb02d5826d2e95e1bd6a4c632c8b5d
abebawu <78111430+abebawu@users.noreply.github.com>
2022-07-07 04:55:40 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,11 @@
 int sum_them_all(const unsigned int n, ...)                                                                                            
 {                                                                                                                                       
 	va_list number;                                                                                                                
-	unsigned int index , sum = 0;                                                                                                           	va_start(number, n);                                                                                                                    
-	for (index=0; index<n; index++)                                                                                                 
-		        sum+= va_arg(number, int);                                                                                      
-	va_end(number);                                                                                                                         	return (sum);                                                                                                                           
+	unsigned int index , sum = 0;                                                                                                           	
+	va_start(number, n);                                                                                                                    
+	for (index=0; index<n; index++) 
+		 sum+= va_arg(number, int);                                                                                      
+	va_end(number);                                                                                                                         	
+	return (sum);                                                                                                                           
 }
 
